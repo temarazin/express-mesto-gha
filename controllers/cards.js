52c3bf8f06bc70b876e83be8f381d5e7c75866dc
temarazin <temarazin@gmail.com>
2022-07-02 18:01:22 +0300

    fix: cards controller fixes

--- controllers/cards.js
+++ controllers/cards.js
@@ -26,11 +26,18 @@ const addCard = (req, res) => {
 
 const deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.send(card))
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
+        const ERROR_CODE = 404;
+        res.status(ERROR_CODE).send({ message: 'Карточка с указанным id не найдена' });
+      }
+    })
     .catch((err) => {
       if (err.path === '_id') {
-        const ERROR_CODE = 404;
-        res.status(ERROR_CODE).send({ message: `Карточка с id ${err.value} не найдена` });
+        const ERROR_CODE = 400;
+        res.status(ERROR_CODE).send({ message: 'Неверный формат id карточки' });
       } else {
         const ERROR_CODE = 500;
         res.status(ERROR_CODE).send({ message: 'Что-то пошло не так' });
@@ -44,14 +51,18 @@ const likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        const ERROR_CODE = 400;
-        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные для постановки лайка.' });
-      } else if (err.path === '_id') {
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
         const ERROR_CODE = 404;
-        res.status(ERROR_CODE).send({ message: `Передан несуществующий id (${err.value}) карточки` });
+        res.status(ERROR_CODE).send({ message: 'Карточка с указанным id не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.path === '_id') {
+        const ERROR_CODE = 400;
+        res.status(ERROR_CODE).send({ message: 'Неверный формат id карточки' });
       } else {
         const ERROR_CODE = 500;
         res.status(ERROR_CODE).send({ message: 'Что-то пошло не так' });
@@ -65,14 +76,18 @@ const dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send(card))
-    .catch((err) => {
-      if (err.name === 'ValidationError') {
-        const ERROR_CODE = 400;
-        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные для снятия лайка.' });
-      } else if (err.path === '_id') {
+    .then((card) => {
+      if (card) {
+        res.send(card);
+      } else {
         const ERROR_CODE = 404;
-        res.status(ERROR_CODE).send({ message: `Передан несуществующий id (${err.value}) карточки` });
+        res.status(ERROR_CODE).send({ message: 'Карточка с указанным id не найдена' });
+      }
+    })
+    .catch((err) => {
+      if (err.path === '_id') {
+        const ERROR_CODE = 400;
+        res.status(ERROR_CODE).send({ message: 'Неверный формат id карточки' });
       } else {
         const ERROR_CODE = 500;
         res.status(ERROR_CODE).send({ message: 'Что-то пошло не так' });
