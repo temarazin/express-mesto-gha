6f5104a8634b591d1c2331daa8d83513302fef6a
temarazin <temarazin@gmail.com>
2022-07-02 14:40:18 +0300

    feat: add updateProfile and updateAvatar functions

--- controllers/users.js
+++ controllers/users.js
@@ -19,4 +19,50 @@ const addUser = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
-module.exports = { getUsers, getUser, addUser };
+const updateProfile = (req, res) => {
+  const { name, about } = req.body;
+  if (name && about) {
+    User.findByIdAndUpdate(
+      req.user._id,
+      { name, about },
+      { new: true, runValidators: true },
+      (err, docs) => {
+        if (err) {
+          res.status(400).send(err.name);
+        } else {
+          res.send(docs);
+        }
+      },
+    );
+  } else {
+    res.status(400).send({ message: 'Неверные параметры' });
+  }
+};
+
+const updateAvatar = (req, res) => {
+  const { avatar } = req.body;
+  if (avatar) {
+    User.findByIdAndUpdate(
+      req.user._id,
+      { avatar },
+      { new: true, runValidators: true },
+      (err, docs) => {
+        if (err) {
+          res.status(400).send(err.name);
+        } else {
+          res.send(docs);
+        }
+      },
+    );
+  } else {
+    res.status(400).send({ message: 'Неверные параметры' });
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUser,
+  addUser,
+  updateProfile,
+  updateAvatar,
+};
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
-const { getUsers, getUser, addUser } = require('../controllers/users');
+const {
+  getUsers,
+  getUser,
+  addUser,
+  updateProfile,
+  updateAvatar,
+} = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUser);
 router.post('/', addUser);
+router.patch('/me', updateProfile);
+router.patch('/avatar', updateAvatar);
 
 module.exports = router;
